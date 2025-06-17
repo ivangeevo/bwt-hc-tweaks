e339d8f0b2d6030cbc4e860bc887a3d3af6d96ae
Ivan Geevo <dbowgaming@gmail.com>
2025-06-17 17:52:59 +0300

    testing reworked soul urn logic to convert entities

---
+++ src/main/java/org/ivangeevo/bwt_hct/mixin/SawBlockMixin.java
@@ -0,0 +1,37 @@
+package org.ivangeevo.bwt_hct.mixin;
+
+
+import com.bwt.blocks.SawBlock;
+import com.bwt.blocks.SimpleFacingBlock;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(SawBlock.class)
+public abstract class SawBlockMixin extends SimpleFacingBlock
+{
+    // The original value from BWT
+    @Unique
+    private static final int sawTimeBaseTickRate = 15;
+
+    // The updated value from retail BTW
+    @Unique
+    private static final int retailSawTimeBaseTickRate = 20;
+
+    protected SawBlockMixin(Settings settings) {
+        super(settings);
+    }
+
+
+    // Modifying the tick rate argument to the retail BTW value
+    @ModifyArg(method = "scheduleUpdateIfRequired",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/World;scheduleBlockTick(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/Block;I)V", ordinal = 1), index = 2)
+    private int modifySawBreakSpeed(int par3) {
+        return retailSawTimeBaseTickRate;
+    }
+
+
+}
