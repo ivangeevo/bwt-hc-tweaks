57698772df6ab61877110209326cb12c9eed4193
ivan <dbowgaming@gmail.com>
2025-07-19 18:15:22 +0300

    bump cleanup

--- src/main/java/org/ivangeevo/bwt_hct/mixin/HempCropBlockMixin.java
+++ src/main/java/org/ivangeevo/bwt_hct/mixin/HempCropBlockMixin.java
@@ -1,20 +1,16 @@
 package org.ivangeevo.bwt_hct.mixin;
 
-import com.bwt.blocks.BwtBlocks;
 import com.bwt.blocks.HempCropBlock;
 import net.minecraft.block.*;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
-import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
-import net.minecraft.world.World;
 import org.ivangeevo.bwt_hct.blocks.HempCropBlockManager;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
